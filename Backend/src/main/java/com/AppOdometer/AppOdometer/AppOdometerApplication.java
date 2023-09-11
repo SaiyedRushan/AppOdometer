fc18cad14915bd2596b1e33b4204b81d57e42e02
SaiyedRushan <rushan52@gmail.com>
2023-09-11 16:45:13 -0400

    Frontend and Backend init

---
+++ Backend/src/main/java/com/AppOdometer/AppOdometer/AppOdometerApplication.java
@@ -0,0 +1,13 @@
+package com.AppOdometer.AppOdometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppOdometerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppOdometerApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/AppOdometer/AppOdometer/AppOdometerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AppOdometer.AppOdometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppOdometerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
